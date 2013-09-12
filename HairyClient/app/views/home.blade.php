07df73b0754e3f699286aa8f84219cda0e6fe4ed
Corey Frenette <corey@narwhunderful.com>
2013-09-12 12:32:22 -0300

    Update home view
    - Show old input if there are validation errors
    - Show login form if guest, or logout/play if logged in

--- HairyClient/app/views/home.blade.php
+++ HairyClient/app/views/home.blade.php
@@ -7,25 +7,31 @@
   </head>
   
   <body>
-    {{ Form::open(['action' => 'AuthController@login', 'method' => 'put']) }}
-    
-    <div class="form-block">
-      {{ Form::text('name', Input::old('name'), ['placeholder' => Lang::get('auth.username'), 'autofocus' => 'autofocus']) }}
+    @if(Auth::guest())
+      {{ Form::open(['action' => 'AuthController@login', 'method' => 'put']) }}
       
-      @foreach($errors->get('name') as $error)
-        {{ $error }}
-      @endforeach
-    </div>
-    
-    <div class="form-block">
-      {{ Form::password('pass', ['placeholder' => Lang::get('auth.password')]) }}
+      <div class="form-block">
+        {{ Form::text('username', Input::old('username'), ['placeholder' => Lang::get('auth.username'), 'autofocus' => 'autofocus']) }}
+        
+        @foreach($errors->get('username') as $error)
+          {{ $error }}
+        @endforeach
+      </div>
       
-      @foreach($errors->get('pass') as $error)
-        {{ $error }}
-      @endforeach
-    </div>
-    
-    {{ Form::submit(Lang::get('auth.login')) }}
-    {{ Form::close() }}
+      <div class="form-block">
+        {{ Form::password('password', ['placeholder' => Lang::get('auth.password')]) }}
+        
+        @foreach($errors->get('password') as $error)
+          {{ $error }}
+        @endforeach
+      </div>
+      
+      {{ Form::submit(Lang::get('auth.login')) }}
+      {{ Form::close() }}
+      {{ HTML::linkAction('HomeController@register', Lang::get('home.register')) }}
+    @else
+      <p>{{ HTML::linkAction('AuthController@logout', Lang::get('auth.logout')) }}</p>
+      <p>{{ HTML::linkAction('Games\SpaceController@home', Lang::get('games.space')) }}</p>
+    @endif
   </body>
 </html>
