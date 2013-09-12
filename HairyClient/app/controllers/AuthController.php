6d9c0eb552d18ca1ce4b5b8e7e9fa3aea8fd9cc5
Corey Frenette <corey@narwhunderful.com>
2013-09-12 12:26:27 -0300

    Auth updates
    - Event listeners for login/logout to generate/nullify auth tokens
    - Require auth for logout, nauth for everything else
    - Change name -> username, pass -> password
    - Change min name length to 5 because I want to be Corey ;)
    - Flash error "Something went wrong" if login fails, but that should
    never happen because of the validation
    - Only flash username on validation fail, we don't need passwords/hidden
    form stuff
    - Add logout function

--- HairyClient/app/controllers/AuthController.php
+++ HairyClient/app/controllers/AuthController.php
@@ -1,42 +1,61 @@
 <?php
 
+Event::listen('auth.login', function($user) {
+  $user->auth = str_random(64);
+  $user->save();
+});
+
+Event::listen('auth.logout', function($user) {
+  $user->auth = null;
+  $user->save();
+});
+
 class AuthController extends Controller {
   public function __construct() {
-    $this->beforeFilter('nauth');
-    $this->beforeFilter('csrf', array('on' => array('post', 'put', 'delete')));
+    $this->beforeFilter('auth', ['only' => 'logout']);
+    $this->beforeFilter('nauth', ['except' => 'logout']);
+    $this->beforeFilter('csrf', ['on' => ['post', 'put', 'delete']]);
   }
   
-  public function putRegister() {
+  public function register() {
     $validator = Validator::make(Input::all(), [
-      'name' => ['required', 'min:6', 'max:20', 'unique:users'],
-      'pass' => ['required', 'min:8', 'max:256']
+      'username' => ['required', 'min:5', 'max:20', 'unique:users'],
+      'password' => ['required', 'min:8', 'max:256']
     ]);
     
     if($validator->passes()) {
       $user = new User;
-      $user->name = Input::get('name');
-      $user->pass = Hash::make(Input::get('pass'));
+      $user->name = Input::get('username');
+      $user->pass = Hash::make(Input::get('password'));
+      $user->auth = str_random(64);
       $user->save();
       Auth::login($user);
       
       return Redirect::action('HomeController@home');
     } else {
-      return Redirect::action('HomeController@home')->withInput(Input::flash())->withErrors($validator);
+      return Redirect::action('HomeController@home')->withInput(Input::only('username'))->withErrors($validator);
     }
   }
   
   public function login() {
     $validator = Validator::make(Input::all(), [
-      'name' => ['required', 'min:6', 'max:20', 'exists:users'],
-      'pass' => ['required', 'min:8', 'max:256']
+      'username' => ['required', 'min:5', 'max:20', 'exists:users'],
+      'password' => ['required', 'min:8', 'max:256']
     ]);
     
     if($validator->passes()) {
-      if(Auth::attempt(Input::all())) {
+      if(Auth::attempt(Input::only('username', 'password'))) {
         return Redirect::action('HomeController@home');
+      } else {
+        return Redirect::action('HomeController@home')->withInput(Input::only('username'))->withErrors(['username' => 'Something went wrong']);
       }
     }
     
-    return Redirect::action('HomeController@home')->withInput(Input::flash())->withErrors($validator);
+    return Redirect::action('HomeController@home')->withInput(Input::only('username'))->withErrors($validator);
+  }
+  
+  public function logout() {
+    Auth::logout();
+    return Redirect::action('HomeController@home');
   }
 }
