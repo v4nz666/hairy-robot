92f76228634e79ec69b7bfe3656600af0a58b78d
Corey Frenette <corey@narwhunderful.com>
2013-09-12 12:32:30 -0300

    Update routes

--- HairyClient/app/routes.php
+++ HairyClient/app/routes.php
@@ -2,3 +2,7 @@
 
 Route::get('/', 'HomeController@home');
 Route::put('/login', 'AuthController@login');
+Route::get('/logout', 'AuthController@logout');
+Route::get('/register', 'HomeController@register');
+Route::put('/register', 'AuthController@register');
+Route::get('/games/space', 'Games\SpaceController@home');
