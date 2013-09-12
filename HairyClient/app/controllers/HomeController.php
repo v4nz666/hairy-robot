8bb5d313fd78c532248a47a3d07c8a88fe09d3f7
Corey Frenette <corey@narwhunderful.com>
2013-09-12 12:27:02 -0300

    Home updates
    - Add register action
    - Require nauth on register
    - Add CSRF protection

--- HairyClient/app/controllers/HomeController.php
+++ HairyClient/app/controllers/HomeController.php
@@ -1,7 +1,16 @@
 <?php
 
 class HomeController extends Controller {
+  public function __construct() {
+    $this->beforeFilter('nauth', ['only' => 'register']);
+    $this->beforeFilter('csrf',  ['on' => ['post', 'put', 'delete']]);
+  }
+  
   public function home() {
     return View::make('home');
+  }
+  
+  public function register() {
+    return View::make('register');
   }
 }
