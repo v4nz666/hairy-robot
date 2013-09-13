ca6d95b4bc5916063be7964daf107fa2bc1fcb54
Corey Frenette <corey@narwhunderful.com>
2013-09-12 23:33:38 -0300

    Simplify msg

--- HairyServer/src/main/Msg.java
+++ HairyServer/src/main/Msg.java
@@ -1,17 +1,11 @@
 package main;
 
 public class Msg {
-  private String _id;
-  private String _msg;
+  public String id;
+  public String msg;
   
-  public Msg() { }
   public Msg(String id, String msg) {
-    _id = id;
-    _msg = msg;
+    this.id = id;
+    this.msg = msg;
   }
-  
-  public String getID()  { return _id; }
-  public String getMsg() { return _msg; }
-  public void setID (String id)  { _id  = id; }
-  public void setMsg(String msg) { _msg = msg; }
 }
