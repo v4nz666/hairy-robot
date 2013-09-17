be9edf3acf027b1ad71b70d23fe197ed1e5d78a1
Corey Frenette <corey@narwhunderful.com>
2013-09-17 08:33:12 -0300

    Add rate of fire, make bullets singletons

--- HairyServer/src/space/data/guns/Bullet.java
+++ HairyServer/src/space/data/guns/Bullet.java
@@ -16,6 +16,7 @@ public abstract class Bullet {
     public float getAcc();
     public float getVel();
     public float getMaxVel();
+    public long getROF();
   }
   
   public static interface Energy {
@@ -24,27 +25,37 @@ public abstract class Bullet {
   
   public static class MachineGun {
     public static class Standard extends Bullet implements Ammo, Ballistic {
-      @Override public String getName()     { return "200mm Light Anti-Spacecraft Rounds"; }
-      @Override public int    getCost()     { return 0; }
-      @Override public int    getLVL()      { return 0; }
-      @Override public int    getClipSize() { return 60; }
-      @Override public int    getDamage()   { return 5; }
-      @Override public int    getRange()    { return 200; }
-      @Override public float  getHeatGen()  { return 2; }
-      @Override public float  getAcc()      { return 0; }
-      @Override public float  getVel()      { return 20; }
-      @Override public float  getMaxVel()   { return 20; }
+      private static Standard _instance = new Standard();
+      public static Standard instance() { return _instance; }
+      private Standard() { }
+      
+      @Override public String getName()       { return "200mm Light Anti-Spacecraft Rounds"; }
+      @Override public int    getCost()       { return 0; }
+      @Override public int    getLVL()        { return 0; }
+      @Override public int    getClipSize()   { return 60; }
+      @Override public int    getDamage()     { return 5; }
+      @Override public int    getRange()      { return 200; }
+      @Override public float  getHeatGen()    { return 2; }
+      @Override public float  getAcc()        { return 0; }
+      @Override public float  getVel()        { return 20; }
+      @Override public float  getMaxVel()     { return 20; }
+      @Override public long   getROF()        { return 250000000l; }
     }
   }
   
   public static class PlasmaTurret {
     public static class Standard extends Bullet implements Ballistic, Energy {
+      private static Standard _instance = new Standard();
+      public static Standard instance() { return _instance; }
+      private Standard() { }
+      
       @Override public int   getDamage()    { return 50; }
       @Override public int   getRange()     { return 300; }
       @Override public float getHeatGen()   { return 50; }
       @Override public float getAcc()       { return 0; }
       @Override public float getVel()       { return 10; }
       @Override public float getMaxVel()    { return 10; }
+      @Override public long  getROF()       { return 1000000000l; }
       @Override public float getEnergyUse() { return 50; }
     }
   }
