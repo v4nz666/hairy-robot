2285cf5dcea29952f303e7205547a8fcfa2c1b96
Corey Frenette <corey@narwhunderful.com>
2013-09-18 12:03:11 -0300

    Why did I put the fire code in the Gun's data class?

--- HairyServer/src/space/data/guns/Gun.java
+++ HairyServer/src/space/data/guns/Gun.java
@@ -3,11 +3,7 @@ package space.data.guns;
 import java.util.LinkedHashMap;
 import java.util.Random;
 
-import main.Server;
-import main.User;
-
 public abstract class Gun {
-  private static Server _server = Server.instance();
   private static Random _rand = new Random();
   private static LinkedHashMap<String, Gun> _registry = new LinkedHashMap<>();
   private static Gun _default;
@@ -35,46 +31,6 @@ public abstract class Gun {
   public abstract int getBarrels();
   public abstract float getBarrelSpread();
   public abstract float getBulletDeviation();
-  
-  protected long _lastBullet;
-  protected long _bulletsLeft;
-  protected int _barrel;
-  protected float _barrelAngle = -getBarrelSpread() * (getBarrels() - 1) / 2;
-  
-  public void fire(User user, Bullet bullet) {
-    Bullet.Ammo      ammo      = bullet instanceof Bullet.Ammo      ? (Bullet.Ammo)     bullet : null;
-    Bullet.Ballistic ballistic = bullet instanceof Bullet.Ballistic ? (Bullet.Ballistic)bullet : null;
-    //Bullet.Energy    energy    = bullet instanceof Bullet.Energy    ? (Bullet.Energy)   bullet : null;
-    
-    if(ballistic != null) {
-      if(_lastBullet <= System.nanoTime()) {
-        _server.addBullet(new space.game.Bullet(user, bullet, getBulletDeviation(), _barrelAngle)); //TODO: Factor in some random with getBulletDeviation()
-        
-        _barrel++;
-        if(_barrel >= getBarrels()) {
-          _barrel = 0;
-          _barrelAngle = -getBarrelSpread() * (getBarrels() - 1) / 2;
-        } else {
-          _barrelAngle += getBarrelSpread();
-        }
-        
-        if(ammo != null) {
-          if(_bulletsLeft == 0) {
-            _bulletsLeft = ammo.getClipSize();
-          }
-          
-          _bulletsLeft--;
-          if(_bulletsLeft > 0) {
-            _lastBullet = System.nanoTime() + ballistic.getROF();
-          } else {
-            _lastBullet = System.nanoTime() + ammo.getReloadTime();
-          }
-        } else {
-          _lastBullet = System.nanoTime() + ballistic.getROF();
-        }
-      }
-    }
-  }
   
   public static class MachineGun extends Gun {
     private static MachineGun _instance = new MachineGun();
