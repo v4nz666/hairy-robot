8919a161aef9fd270a28970eef2c67ff3aecfb6b
Corey Frenette <corey@narwhunderful.com>
2013-10-01 22:18:48 -0300

    Build needs to be first or it gets picked up as an IP

--- HairyClient/app/routes.php
+++ HairyClient/app/routes.php
@@ -5,5 +5,5 @@ Route::put('/login',                'AuthController@login');
 Route::get('/logout',               'AuthController@logout');
 Route::get('/register',             'HomeController@register');
 Route::put('/register',             'AuthController@register');
-Route::get('/games/space/{ip?}/{port?}', 'games\SpaceController@home');
 Route::get('/games/space/build',    'games\SpaceController@build');
+Route::get('/games/space/{ip?}/{port?}', 'games\SpaceController@home');
