bb83f915d28a666092916714156ca250623f3c78
Corey Frenette <corey@narwhunderful.com>
2013-10-08 13:31:08 -0300

    A very cool method that not only extends a superclass but allows access to it via this.uber

--- HairyClient/public/assets/js/helper.js
+++ HairyClient/public/assets/js/helper.js
@@ -24,6 +24,30 @@ function getTextHeight(font) {
   return result;
 };
 
+Function.prototype.inherits = function(parent) {
+  var d = 0, p = (this.prototype = new parent());
+ 
+  this.prototype.uber = function(name) {
+    var f, r, t = d, v = parent.prototype;
+    if (t) {
+      while (t) {
+        v = v.constructor.prototype;
+        t -= 1;
+      }
+      f = v[name];
+    } else {
+      f = p[name];
+      if (f == this[name]) {
+        f = v[name];
+      }
+    }
+    d += 1;
+    r = f.apply(this, Array.prototype.slice.apply(arguments, [1]));
+    d -= 1;
+    return r;
+  };
+};
+
 if(!Array.prototype.indexOf) {
   Array.prototype.indexOf = function(elt /*, from*/) {
     var len = this.length >>> 0;
