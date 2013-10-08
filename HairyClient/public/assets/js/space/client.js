3ba0fb914bea264d39c9c74a19da65212d00da0d
Corey Frenette <corey@narwhunderful.com>
2013-10-07 23:30:23 -0300

    Split helper funcs into their own js file

--- HairyClient/public/assets/js/space/client.js
+++ HairyClient/public/assets/js/space/client.js
@@ -879,32 +879,6 @@ function User() {
   }
 }
 
-function getTextHeight(font) {
-  var text = $('<span>Hg</span>').css({ fontFamily: font });
-  var block = $('<div style="display: inline-block; width: 1px; height: 0px;"></div>');
-  var div = $('<div></div>');
-  var body = $('body');
-  
-  div.append(text, block);
-  body.append(div);
-  
-  try {
-    var result = {};
-    
-    block.css({ verticalAlign: 'baseline' });
-    result.ascent = block.offset().top - text.offset().top;
-    
-    block.css({ verticalAlign: 'bottom' });
-    result.height = block.offset().top - text.offset().top;
-    
-    result.descent = result.height - result.ascent;
-  } finally {
-    div.remove();
-  }
-  
-  return result;
-};
-
 $(document).ready(function() {
   var client = Client();
   client.init();
