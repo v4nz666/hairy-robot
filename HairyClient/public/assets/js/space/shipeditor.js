ca905912f29fef0f9f4466ebbc3dcff09dfb92c5
Corey Frenette <corey@narwhunderful.com>
2013-10-13 23:29:39 -0300

    Work on ship editor

--- HairyClient/public/assets/js/space/shipeditor.js
+++ HairyClient/public/assets/js/space/shipeditor.js
@@ -1,13 +1,26 @@
 function ShipEditor(ctx) {
   return {
     ship: null,
+    part: null,
     
     create: function(ctx) {
       var priv = this;
       
       var me = GUI(ctx);
       me.init = function() {
+        $.ajax({
+          url: '/games/store/parts',
+          dataType: 'json',
+        }).done(function(data) { priv.part = data; })
+          .fail(function() { console.log('Failed to get parts'); });
+        
         priv.ship = Ship();
+        
+        var list = List(this);
+        list.h = 500;
+        list.w = 500;
+        list.items().push('Test');
+        this.controls.add(list);
       }
       
       me.init();
