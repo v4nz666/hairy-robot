90caa2d90da787490916bdec85271f34d91a815c
v4nz666 <666v4nz@gmail.com>
2013-10-14 19:19:47 -0300

    Standardize stystem size to 2^27. Closes #48

--- HairyServer/src/main/Star_System.java
+++ HairyServer/src/main/Star_System.java
@@ -20,9 +20,8 @@ public class Star_System {
     this._name = generateName();
     System.out.println("Generating System[" + this._name + "]");
     
-    // 2^27 x 32m/coordinate ~= 1/1000 scale our Solar system, so range from half
-    //  that to twice that.
-    _size = (int)Math.pow(2, 26 + _rand.nextInt(3));
+    // 2^27 x 32m/coordinate ~= 1/1000 scale our Solar system
+    _size = (int)Math.pow(2, 27);
     System.out.println("System Size[" + (long)_size * 32 + "m][" + _size + "px]");
     
     this.star = Star.Generate(this);
