73e0276a11c6380eb6ec3a3fc189ec33a87036c0
Corey Frenette <corey@narwhunderful.com>
2013-10-14 23:11:32 -0300

    Add info frame, add first ship part, render ship

--- HairyClient/public/assets/js/space/shipeditor.js
+++ HairyClient/public/assets/js/space/shipeditor.js
@@ -16,28 +16,41 @@ function ShipEditor(ctx) {
           priv.part = data;
           
           for(var i = 0; i < priv.part.length; i++) {
-            var item = list.items().push(priv.part[i].name + ' - ' + priv.part[i].desc);
+            var item = lstParts.items().push(priv.part[i].name + ' - ' + priv.part[i].desc);
             item.part = priv.part[i];
             item.onselect = itemsel;
           }
+          
+          priv.ship.addPart(0, 0, priv.part[0]);
         }).fail(function() {
           console.log('Failed to get parts');
         });
         
         priv.ship = Ship();
         
-        var list = List(this);
-        list.w = 250;
-        
         var itemsel = function(item) {
           priv.selected = item.part;
         };
         
-        this.controls.add(list);
+        var fraInfo = Frame(this);
+        fraInfo.w = 250;
+        fraInfo.h = 150;
+        
+        var lstParts = List(this);
+        lstParts.w = fraInfo.w;
+        
+        this.controls.add(fraInfo);
+        this.controls.add(lstParts);
         
         this.onresize = function() {
-          list.h = ctx.canvas.height;
-          list.x = ctx.canvas.width - list.w;
+          fraInfo.x = ctx.canvas.width - lstParts.w;
+          lstParts.x = fraInfo.x;
+          lstParts.y = fraInfo.h;
+          lstParts.h = ctx.canvas.height - fraInfo.h;
+        };
+        
+        this.onrender = function() {
+          priv.ship.render(ctx);
         };
       }
       
