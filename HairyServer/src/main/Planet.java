483248767ac0dbd7967dca55a1ac04215143e664
v4nz666 <666v4nz@gmail.com>
2013-10-14 23:58:20 -0300

    GH-52 Add Moon stub

--- HairyServer/src/main/Planet.java
+++ HairyServer/src/main/Planet.java
@@ -5,16 +5,41 @@ import java.util.Random;
 public class Planet extends Entity {
   private static Random _rand = new Random();
   
+  private Star_System _system;
+  
   private int _mass;
   private int _distance;
   
-  private static int generateSize(int d, Star_System _system) {
+  private Moon[] _moons;
+  
+  public Planet(Star_System _system, int d) {
+    super(-1,0,0,0);
+    this._system = _system;
+    this._distance = d;
+    this.size = generateSize(this);
+    
+    System.out.println("Generating Planet at distance [" + d + "]");
+    
+    // Select a random spot in the orbit to generate our planet at
+    int theta = _rand.nextInt(360);
+    int mid = _system.getSize() / 2;
+    
+    this.x = Math.floor(mid + (Math.cos(theta * Math.PI / 180) * d));
+    this.y = Math.floor(mid - (Math.sin(theta * Math.PI / 180) * d));
+    System.out.println("Planet: size [" + this.size + "] at[" + theta + "]degrees " + 
+        "[" + (int)this.x + ", " + (int)this.y + "]");
+    this._moons = generateMoons(this);
+    
+  }
+  
+  private static int generateSize(Planet p) {
     
     int scale = _rand.nextInt(10);
     int div;
-    
+    int d = p._distance;
+    int sSize = p._system.getSize();
     // Constrain d so that inner/outer planets stay a reasonable size
-    d = Math.max(Math.min(d, _system.getSize() / 15), _system.getSize() / 100);
+    d = Math.max(Math.min(d, sSize / 15), sSize / 100);
     
     // Small planets
     if ( scale <= 2 ) {
@@ -31,25 +56,9 @@ public class Planet extends Entity {
     
     return (int)((d / div) + (d / (div * 3) * _rand.nextFloat() - d/(div * 6)));
   }
-  
-  public Planet(Star_System _system, int d) {
-    this(-1, 0, 0, generateSize(d, _system));
-    
-    this._distance = d;
-    
-    System.out.println("Generating Planet at distance [" + d + "]");
-    
-    // Select a random spot in the orbit to generate our planet at
-    int theta = _rand.nextInt(360);
-    int mid = _system.getSize() / 2;
-    
-    this.x = Math.floor(mid + (Math.cos(theta * Math.PI / 180) * d));
-    this.y = Math.floor(mid - (Math.sin(theta * Math.PI / 180) * d));
-    System.out.println("Planet: size [" + this.size + "] at[" + theta + "]degrees " + 
-        "[" + (int)this.x + ", " + (int)this.y + "]");
+  private static Moon[] generateMoons(Planet p) {
+    // TODO Auto-generated method stub
+    return null;
   }
-  
-  public Planet(int id, double x, double y, int size) {
-    super(id, x, y, size);
-  }
+
 }
