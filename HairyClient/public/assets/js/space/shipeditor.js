cea76e8be6a51c38009ab61490cedddd8427fd92
Corey Frenette <corey@narwhunderful.com>
2013-10-15 11:29:38 -0300

    More work on #5

--- HairyClient/public/assets/js/space/shipeditor.js
+++ HairyClient/public/assets/js/space/shipeditor.js
@@ -2,6 +2,14 @@ function ShipEditor(ctx) {
   return {
     ship: null,
     selected: null,
+    hover: null,
+    
+    halfW: 0,
+    halfH: 0,
+    mouseX: 0,
+    mouseY: 0,
+    gridX: 0,
+    gridY: 0,
     
     create: function(ctx) {
       var priv = this;
@@ -36,10 +44,44 @@ function ShipEditor(ctx) {
           lstParts.x = fraInfo.x;
           lstParts.y = fraInfo.h;
           lstParts.h = ctx.canvas.height - fraInfo.h;
+          
+          priv.halfW = ctx.canvas.width  / 2;
+          priv.halfH = ctx.canvas.height / 2;
         };
         
         this.onrender = function() {
+          ctx.save();
+          ctx.translate(priv.halfW, priv.halfH);
           priv.ship.render(ctx);
+          ctx.restore();
+        };
+        
+        this.onmousemove = function(ev) {
+          priv.mouseX = ev.offsetX - priv.halfW;
+          priv.mouseY = ev.offsetY - priv.halfH;
+          
+          var gridX = Math.floor((ev.offsetX - priv.halfW) / 16);
+          var gridY = Math.floor((ev.offsetY - priv.halfH) / 16);
+          
+          if(gridX != priv.gridX || gridY != priv.gridY) {
+            priv.gridX = gridX;
+            priv.gridY = gridY;
+            priv.hover = priv.ship.partAt(priv.gridX, priv.gridY);
+            
+            if(ev.which != 0) {
+              me.click(ev);
+            }
+          }
+        };
+        
+        this.onclick = function(ev) {
+          var x = priv.gridX;
+          var y = priv.gridY;
+          
+          if(priv.ship.addPart(x, y, priv.selected)) {
+            //if(x < 0) { this.startX += x; }
+            //if(y < 0) { this.startY += y; }
+          }
         };
       }
       
