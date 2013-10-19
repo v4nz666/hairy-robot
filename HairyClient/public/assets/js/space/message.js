bc66b176c06733f8f44802795be008bc50d05c62
Corey Frenette <corey@narwhunderful.com>
2013-10-18 22:48:05 -0300

    Add basic message box GUI

---
+++ HairyClient/public/assets/js/space/message.js
@@ -0,0 +1,31 @@
+function Message(ctx, text) {
+  return {
+    create: function() {
+      var priv = this;
+      
+      var me = GUI(ctx);
+      me.init = function() {
+        var l = Label(this);
+        l.textAlign = 'center';
+        l.textBaseline = 'middle';
+        l.autosize = false;
+        l.text(text);
+        l.w = 150;
+        
+        var f = Frame(this);
+        f.w = l.w;
+        f.h = 50;
+        f.controls.add(l);
+        
+        this.controls.add(f);
+        
+        this.onresize = function() {
+          f.x = (ctx.canvas.width  - f.w) / 2;
+          f.y = (ctx.canvas.height - f.h) / 2;
+        };
+      };
+      
+      return me;
+    }
+  }.create();
+}
