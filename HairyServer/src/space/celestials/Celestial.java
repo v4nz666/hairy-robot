0842d27eb4d6fe611ef635c5cc13d99f4ef1020d
Corey Frenette <corey@narwhunderful.com>
2013-10-23 13:42:44 -0300

    Should probably be -1 for now

--- HairyServer/src/space/celestials/Celestial.java
+++ HairyServer/src/space/celestials/Celestial.java
@@ -4,6 +4,6 @@ import main.Entity;
 
 public abstract class Celestial extends Entity {
   public Celestial(double x, double y, int size) {
-    super(0, x, y, size, -1);
+    super(-1, x, y, size, -1);
   }
 }
