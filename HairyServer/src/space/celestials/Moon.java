670d746099a5d67d23e7d33a3e8987c2e05d3118
Corey Frenette <corey@narwhunderful.com>
2013-10-23 22:24:50 -0300

    Proper Moon generation

--- HairyServer/src/space/celestials/Moon.java
+++ HairyServer/src/space/celestials/Moon.java
@@ -5,25 +5,16 @@ import java.util.Random;
 public class Moon extends Celestial {
   private static Random _rand = new Random(); 
   
-  private Planet _planet;
-  
-  private double th;
-  
-  public Moon(StarSystem s, Planet p, int seq) {
-    super(0, 0, -1);
-    _system = s;
-    _planet = p;
-    
-    th = 360 * _rand.nextDouble();
-    int pS = _planet.size;
-    
-    int d = Math.max(pS * 3, pS * seq) + (pS * seq / 2);
-    
-    x = _planet.x + Math.cos(th * (Math.PI / 2)) * d;
-    y = _planet.y + Math.sin(th * (Math.PI / 2)) * d;
-    
+  public static Moon generate(StarSystem system, Celestial parent, double distance) {
     int minSize = (int)(512 + (_rand.nextDouble() * 100 - 50)); 
-    this.size = Math.max(minSize, _rand.nextInt(_planet.size / 8));
-    System.out.println("pS[" + pS + "] d[" + d + "] size[" + size + "]");
+    int size = Math.max(minSize, _rand.nextInt(parent.size / 8));
+    
+    return new Moon(system, parent, distance, size, 0, 0);
+  }
+  
+  private Moon(StarSystem system, Celestial parent, double distance, int size, float mass, int temp) {
+    super(system, parent, distance, size, mass, temp);
+    
+    //System.out.println("pS[" + pS + "] d[" + d + "] size[" + size + "]");
   }
 }
