adf675cbb2f2914a6dea8b3e033fefed0155da95
Corey Frenette <corey@narwhunderful.com>
2013-10-23 22:29:04 -0300

    Fake AsteroidBelt gen

--- HairyServer/src/space/celestials/AsteroidBelt.java
+++ HairyServer/src/space/celestials/AsteroidBelt.java
@@ -3,9 +3,13 @@ package space.celestials;
 import main.Asteroid;
 
 public class AsteroidBelt extends Celestial {
-  public AsteroidBelt(StarSystem starSystem, double d) {
-    super(0, 0, 0);
+  public static AsteroidBelt generate(StarSystem system, Celestial parent, double distance) {
+    return new AsteroidBelt(system, parent, distance, 0, 0, 0);
+  }
+  
+  private AsteroidBelt(StarSystem system, Celestial parent, double distance, int size, float mass, int temp) {
+    super(system, parent, distance, size, mass, temp);
   }
 
-  private Asteroid[] _asteroids;
+  //private Asteroid[] _asteroids;
 }
