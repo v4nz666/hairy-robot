8798ebb3fc004b7a67e9bbbe9da9ae7b95288ba6
Corey Frenette <corey@narwhunderful.com>
2013-10-23 22:23:52 -0300

    Add parent to Celestial

--- HairyServer/src/space/celestials/Celestial.java
+++ HairyServer/src/space/celestials/Celestial.java
@@ -11,23 +11,26 @@ public abstract class Celestial extends Entity {
   protected ArrayList<Celestial> _celestial = new ArrayList<>();
   
   protected StarSystem _system;
+  protected Celestial _parent;
   protected double _distance;
   protected double _mass;
   protected double _temp;
   
-  public Celestial(StarSystem system, double distance, int size, double mass, double temp) {
+  public Celestial(StarSystem system, Celestial parent, double distance, int size, double mass, double temp) {
     super(-1, 0, 0, size);
     _system = system;
+    _parent = parent;
     _distance = distance;
     _mass = mass;
     _temp = temp;
     
     // Select a random spot in the orbit to generate at
     int theta = _rand.nextInt(360);
-    double mid = _system.getSize() / 2;
+    double px = _parent != null ? _parent.x : _system.getSize() / 2;
+    double py = _parent != null ? _parent.y : _system.getSize() / 2;
     
-    x = mid + Math.cos(theta * Math.PI / 180) * distance;
-    y = mid - Math.sin(theta * Math.PI / 180) * distance;
+    x = px + Math.cos(theta * Math.PI / 180) * distance;
+    y = py - Math.sin(theta * Math.PI / 180) * distance;
   }
   
   public boolean addCelestial(Celestial c) {
