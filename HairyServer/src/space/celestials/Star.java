c529f0f30ea9c65cd528c10c8610abc1f57ac76e
Corey Frenette <corey@narwhunderful.com>
2013-10-23 22:24:28 -0300

    Add parent to Star

--- HairyServer/src/space/celestials/Star.java
+++ HairyServer/src/space/celestials/Star.java
@@ -8,7 +8,7 @@ public class Star extends Celestial {
   public double getMass() { return _mass; }
   public double getTemp() { return _temp; }
   
-  public static final Star generate(StarSystem system, double distance) {
+  public static final Star generate(StarSystem system, Celestial parent, double distance) {
     // Radius ~= .5 - 5 solar radii (at 1/1000 scale)
     int radius = 12000 + _rand.nextInt(100000);
     
@@ -18,11 +18,11 @@ public class Star extends Celestial {
     // Temperatures range from 300K to 30000K
     int temp = 3000 + 1000 * _rand.nextInt(27);
     
-    return new Star(system, distance, radius, mass, temp);
+    return new Star(system, parent, distance, radius, mass, temp);
   }
   
-  private Star(StarSystem system, double distance, int size, float mass, int temp) {
-    super(system, distance, size, mass, temp);
+  private Star(StarSystem system, Celestial parent, double distance, int size, float mass, int temp) {
+    super(system, parent, distance, size, mass, temp);
     
     System.out.println("Star Coords[" + x + "," + y + "]");
     System.out.println("Star Size[" + (int)size + "]");
