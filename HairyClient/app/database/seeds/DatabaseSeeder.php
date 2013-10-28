4a1604cb5fa42516624dad7867ce2da2c3e7048f
Corey Frenette <corey@narwhunderful.com>
2013-10-28 13:37:27 -0300

    Seed a thruster

--- HairyClient/app/database/seeds/DatabaseSeeder.php
+++ HairyClient/app/database/seeds/DatabaseSeeder.php
@@ -31,7 +31,8 @@ class UserTableSeeder extends Seeder {
 
 class SpacePartTypeTableSeeder extends Seeder {
   public function run() {
-    $types = [['Hull', 'Parts that make up the body of your ship']];
+    $types = [['Hull',     'Parts that make up the body of your ship'],
+              ['Thruster', 'Parts that make you move']];
     
     foreach($types as $type) {
       SpacePartType::create(['name' => $type[0], 'desc' => $type[1]]);
@@ -41,10 +42,20 @@ class SpacePartTypeTableSeeder extends Seeder {
 
 class SpacePartTableSeeder extends Seeder {
   public function run() {
-    $parts = [['Hull', SpacePartType::first()->id, 'A 16x16m² section of hull', 1, 'ctx.fillStyle=\'white\';ctx.strokeStyle=\'grey\';ctx.beginPath();ctx.moveTo(0,0);ctx.lineTo(0,16);ctx.lineTo(16,16);ctx.lineTo(16,0);ctx.closePath();ctx.fill();ctx.stroke();', 1000]];
+    $id = SpacePartType::first()->id;
+    $parts = [['Hull', $id++, 'A 16x16m² section of hull', 1, 'ctx.fillStyle=\'white\';ctx.strokeStyle=\'grey\';ctx.beginPath();ctx.moveTo(0,0);ctx.lineTo(0,16);ctx.lineTo(16,16);ctx.lineTo(16,0);ctx.closePath();ctx.fill();ctx.stroke();', 1000, [], []],
+              ['Rocket', $id++, 'Makes you move', 1, 'ctx.fillStyle=\'white\';ctx.beginPath();ctx.moveTo(4,4);ctx.lineTo(4,12);ctx.lineTo(12,12);ctx.lineTo(12,4);ctx.closePath();ctx.fill();', 1000, [], [['acc', 'Acceleration', 'Acceleration (m/s²)', 'num', '0.03125']]]];
     
     foreach($parts as $part) {
-      SpacePart::create(['name' => $part[0], 'space_part_type_id' => $part[1], 'desc' => $part[2], 'mass' => $part[3], 'render' => $part[4], 'cost' => $part[5]]);
+      $id = SpacePart::create(['name' => $part[0], 'space_part_type_id' => $part[1], 'desc' => $part[2], 'mass' => $part[3], 'render' => $part[4], 'cost' => $part[5]])->id;
+      
+      foreach($part[6] as $info) {
+        SpacePartInfo::create(['space_part_id' => $id, 'var' => $info[0], 'name' => $info[1], 'desc' => $info[2]]);
+      }
+      
+      foreach($part[7] as $attrib) {
+        SpacePartAttrib::create(['space_part_id' => $id, 'var' => $attrib[0], 'name' => $attrib[1], 'desc' => $attrib[2], 'type' => $attrib[3], 'vals' => $attrib[4]]);
+      }
     }
   }
 }
