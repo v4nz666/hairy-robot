be811f6f4cef378d3cb303c3d9352720abc6079c
v4nz666 <666v4nz@gmail.com>
2013-10-28 21:51:25 -0300

    GH-50 add Asteroid Class and Coord class

---
+++ HairyServer/src/space/celestials/Asteroid.java
@@ -0,0 +1,38 @@
+package space.celestials;
+
+public class Asteroid  extends Celestial{
+  @Override
+  public String toString() {
+    return "Asteroid " + super.toString();
+  }
+  
+  private Coord[] _points;
+  
+  public static Asteroid generate(StarSystem system, double distance, double x, double y, int size, Coord[] coords) {
+    Asteroid a = new Asteroid(system, distance, size);
+    
+    // Override the random x/y coordinates with those provided by the Asteroid Belt
+    a.x = system.star.x + x;
+    a.y = system.star.y + y;
+    a._points = coords;
+    return a;
+  }
+  
+  private Asteroid(StarSystem system, double distance, int size) {
+    super(system, system.star, distance, size, 0, 0);
+  }
+
+  @Override
+  public String getFill() {
+    return "#333333";
+  }
+  
+  @Override
+  public String getStroke() {
+    return "#444444";
+  }
+  
+  public Coord[] getPoints() {
+    return _points;
+  }
+}
---
+++ HairyServer/src/space/celestials/Coord.java
@@ -0,0 +1,14 @@
+package space.celestials;
+
+public class Coord {
+  public final double x, y;
+  @Override
+  public String toString() {
+    return "x:" + x + " y: " + y; 
+  }
+  
+  public Coord(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+}
