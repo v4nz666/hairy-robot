bb4e4d93bcfcc0fd20c943c0d5d80b8e42b6b2c3
Corey Frenette <corey@narwhunderful.com>
2013-11-08 21:45:09 -0400

    Remove dummy logger shenanigans

--- HairyServer/src/main/Main.java
+++ HairyServer/src/main/Main.java
@@ -1,12 +1,9 @@
 package main;
 
 import space.Server;
-import util.LoggerFactory;
 
 public class Main {
   public static void main(String[] args) throws InterruptedException, InstantiationException, IllegalAccessException {
-    LoggerFactory.getLogger().enableInfo = false;
-    LoggerFactory.getLogger().enableTrace = false;
     Server.instance().start();
   }
 }
