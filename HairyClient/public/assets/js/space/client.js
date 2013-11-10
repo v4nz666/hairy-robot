fdb48f3b7cb414351c503202d6ef31635668d76d
Corey Frenette <corey@narwhunderful.com>
2013-11-10 10:04:12 -0400

    Remove old main menu

--- HairyClient/public/assets/js/space/client.js
+++ HairyClient/public/assets/js/space/client.js
@@ -51,42 +51,8 @@ function Client() {
             priv.guis.resize();
           };
           
-          var initMenu = function() {
-            var menu = MainMenu(priv.ctx);
-            var name = $('input[name=username]').val();
-            var auth = $('input[name=auth]').val();
-            var ip   = $('input[name=ip]').val();
-            var port = $('input[name=port]').val();
-            
-            menu.onplay = function() {
-              var msg = Message(priv.ctx, 'Connecting...');
-              priv.guis.push(msg);
-              
-              priv.socket = io.connect(ip + ':' + port, {'reconnect': false});
-              priv.socket.on('connect', function() {
-                msg.text('Logging in...');
-                priv.socket.emit('login', {name: name, auth: auth});
-              });
-            };
-            
-            priv.guis.push(menu);
-          };
-          
-          stat.load([{type: 'parts', cb: function() {
-            var draw = function(ctx, render) {
-              eval(this.render);
-            }
-            
-            for(var i = 0; i < stat.parts.length; i++) {
-              stat.parts[i].draw = draw;
-            }
-          }}]);
-          
           priv.canvas = $('#canvas')[0];
           priv.ctx = canvas.getContext('2d');
-          
-          var frameRate = 60;
-          var tickRate = 1000 / frameRate;
           
           // Hook our events
           $(document).bind     ('contextmenu', function() { return false; });
@@ -98,23 +64,31 @@ function Client() {
           $(document).mouseup  ($.proxy(priv.guis.mouseup  , priv.guis));
           $(window)  .resize   (resize);
           
-          setInterval(render , tickRate);
+          setInterval(render , 1000 / 60);
           setInterval(calcfps, 1000);
           
           resize();
           
-          stat.onload(initMenu);
-        },
-        
-        setParams: function(data){
-          this.me = this.user[data.id];
-          console.log('Set id[', this.me.id, ']');
-        },
-        
-        setSystem: function(data){
-          this.system = data.system;
-          this.setZoom(this.zoomLevel);
-          console.log('Set system[', this.system, ']');
+          var name = $('input[name=username]').val();
+          var auth = $('input[name=auth]').val();
+          var ip   = $('input[name=ip]').val();
+          var port = $('input[name=port]').val();
+          
+          var msg = Message(priv.ctx, 'Connecting...');
+          priv.guis.push(msg);
+          
+          priv.socket = io.connect(ip + ':' + port, {'reconnect': false});
+          priv.socket.on('connect', function() {
+            msg.text('Logging in...');
+            priv.socket.emit('lo', {name: name, auth: auth});
+          });
+          
+          priv.socket.on('lr', function() {
+            msg.pop();
+            
+            var guiGame = Game(priv.ctx, priv.socket);
+            priv.guis.push(guiGame);
+          });
         }
       };
       
