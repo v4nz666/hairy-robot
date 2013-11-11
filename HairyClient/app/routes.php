b307cfc7be494f354737dc3125559b8b11d6a12f
Corey Frenette <corey@narwhunderful.com>
2013-11-11 16:34:17 -0400

    Update storage controller route

--- HairyClient/app/routes.php
+++ HairyClient/app/routes.php
@@ -11,7 +11,7 @@ Route::group(['prefix' => 'games'], function() {
     Route::get('/store/credits',         ['as' => 'games_space_store_credits',  'uses' => 'games\space\StorageController@credits']);
     Route::get('/store/types',           ['as' => 'games_space_store_types',    'uses' => 'games\space\StorageController@types']);
     Route::get('/store/parts/{type?}',   ['as' => 'games_space_store_parts',    'uses' => 'games\space\StorageController@parts'])->where('type', '[0-9]+');
-    Route::get('/store/ships',           ['as' => 'games_space_store_ships',    'uses' => 'games\space\StorageController@ships']);
+    Route::get('/store/ships/{type?}',   ['as' => 'games_space_store_ships',    'uses' => 'games\space\StorageController@ships']);
     Route::put('/store/saveship',        ['as' => 'games_space_store_saveship', 'uses' => 'games\space\StorageController@saveship']);
     Route::get('/{ip?}/{port?}',         ['as' => 'games_space',                'uses' => 'games\space\GameController@home']);
   });
