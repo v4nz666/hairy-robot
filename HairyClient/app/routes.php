4ab013e21db751d09582293f502c0671d1fc6eed
Corey Frenette <corey@narwhunderful.com>
2013-11-27 16:10:47 -0400

    Group up /store/

--- HairyClient/app/routes.php
+++ HairyClient/app/routes.php
@@ -1,18 +1,21 @@
 <?php
 
-Route::get('/',                          ['as' => 'home',     'uses' => 'HomeController@home']);
-Route::put('/login',                     ['as' => 'login',    'uses' => 'AuthController@login']);
-Route::get('/logout',                    ['as' => 'logout',   'uses' => 'AuthController@logout']);
-Route::get('/register',                  ['as' => 'register', 'uses' => 'HomeController@register']);
-Route::put('/register',                  'AuthController@register');
+Route::get('/',         ['as' => 'home',     'uses' => 'HomeController@home']);
+Route::put('/login',    ['as' => 'login',    'uses' => 'AuthController@login']);
+Route::get('/logout',   ['as' => 'logout',   'uses' => 'AuthController@logout']);
+Route::get('/register', ['as' => 'register', 'uses' => 'HomeController@register']);
+Route::put('/register', 'AuthController@register');
 
 Route::group(['prefix' => 'games'], function() {
   Route::group(['prefix' => 'space'], function() {
-    Route::get('/store/credits',         ['as' => 'games_space_store_credits',  'uses' => 'games\space\StorageController@credits']);
-    Route::get('/store/types',           ['as' => 'games_space_store_types',    'uses' => 'games\space\StorageController@types']);
-    Route::get('/store/parts/{type?}',   ['as' => 'games_space_store_parts',    'uses' => 'games\space\StorageController@parts'])->where('type', '[0-9]+');
-    Route::get('/store/ships/{type?}',   ['as' => 'games_space_store_ships',    'uses' => 'games\space\StorageController@ships']);
-    Route::put('/store/saveship',        ['as' => 'games_space_store_saveship', 'uses' => 'games\space\StorageController@saveship']);
-    Route::get('/{ip?}/{port?}',         ['as' => 'games_space',                'uses' => 'games\space\GameController@home']);
+    Route::group(['prefix' => 'store'], function() {
+      Route::get('/credits',       ['as' => 'games_space_store_credits',  'uses' => 'games\space\StorageController@credits']);
+      Route::get('/types',         ['as' => 'games_space_store_types',    'uses' => 'games\space\StorageController@types']);
+      Route::get('/parts/{type?}', ['as' => 'games_space_store_parts',    'uses' => 'games\space\StorageController@parts'])->where('type', '[0-9]+');
+      Route::get('/ships/{type?}', ['as' => 'games_space_store_ships',    'uses' => 'games\space\StorageController@ships']);
+      Route::put('/saveship',      ['as' => 'games_space_store_saveship', 'uses' => 'games\space\StorageController@saveship']);
+    });
+    
+    Route::get('/{ip?}/{port?}', ['as' => 'games_space', 'uses' => 'games\space\GameController@home']);
   });
 });
