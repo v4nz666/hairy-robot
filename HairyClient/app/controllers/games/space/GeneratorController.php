2ae7c6feebbf4e84b4b23499f6a40aaa73a34fd3
Corey Frenette <corey@narwhunderful.com>
2013-12-02 19:51:29 -0400

    Simple planet name gen that uses the first 50 extra-solar planets and prepends 'New' 20% of the time...
    Also contains syllable detection code capable of counting syllables but
    not quite capable of splitting words into syllables :(

--- HairyClient/app/controllers/games/space/GeneratorController.php
+++ HairyClient/app/controllers/games/space/GeneratorController.php
@@ -64,6 +64,52 @@ class GeneratorController extends \Controller {
   }
   
   public function planets() {
-    return 'Not implemented';
+    $name = [
+      'Ceres', 'Pallas', 'Juno', 'Vesta', 'Astraea', 'Hebe', 'Iris', 'Flora', 'Metis',
+      'Hygiea', 'Parthenope', 'Victoria', 'Egeria', 'Irene', 'Eunomia', 'Psyche', 'Tethis',
+      'Melpomene', 'Fortuna', 'Massalia', 'Lutetia', 'Kalliope', 'Thalia', 'Themis', 'Procaea',
+      'Proserpina', 'Euterpe', 'Bellona', 'Amphitrite', 'Urania', 'Euphrosyne', 'Pomona',
+      'Polyhymnia', 'Circe', 'Leukothea', 'Atalante', 'Fides', 'Leda', 'Laetitia', 'Harmonia',
+      'Daphne', 'Isis', 'Ariadne', 'Nysa', 'Eugenia', 'Hestia', 'Aglaja', 'Doris', 'Pales', 'Virginia'
+    ];
+    
+    $n = $name[mt_rand(0, count($name) - 1)];
+    
+    if(mt_rand(0, 4) === 0) {
+      $n = 'New ' . $n;
+    }
+    
+    return $n;
+  }
+  
+  // Damn, this was a good shot at splitting words into syllables, but won't work...
+  public function syllables($word) {
+    $word = strtolower($word);
+    
+    // Regex Patterns Needed
+    $triples = 'dn\'t|eau|iou|ouy|you|bl$';
+    $doubles = 'ai|ae|ay|au|ea|ee|ei|eu|ey|ie|ii|io|oa|oe|oi|oo|ou|oy|ue|uy|ya|ye|yi|yo|yu';
+    $singles = 'a|e|i|o|u|y';
+    $vowels = '/('.$triples.'|'.$doubles.'|'.$singles.')/';
+    $trailing_e = '/e$/'; $trailing_s = '/s$/';
+    
+    // Cleaning up word endings
+    $word = preg_replace($trailing_s, '', $word);
+    $word = preg_replace($trailing_e, '', $word);
+    
+    // Count # of “vowels”
+    preg_match_all($vowels, $word, $matches);
+    
+    $i = 0;
+    foreach($matches[0] as $v) {
+      echo $i;
+      $n = strpos($word, $v, $i);
+      echo substr($word, $i, strlen($v) + $n - $i) . ' ';
+      $i += $n + strlen($v);
+    }
+    
+    //$syl_count = count($matches[0]);
+    //return $syl_count;
+    //return $matches;
   }
 }
