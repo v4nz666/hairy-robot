c064cbbea201150324a1a1806f7d1f7711134f6d
Corey Frenette <corey@narwhunderful.com>
2013-12-02 18:56:48 -0400

    Very basic generator...

--- HairyClient/app/controllers/games/space/GeneratorController.php
+++ HairyClient/app/controllers/games/space/GeneratorController.php
@@ -8,6 +8,58 @@ class GeneratorController extends \Controller {
   }
   
   public function ships() {
+    switch(mt_rand(0, 1)) {
+      case 0:
+        $noun = [
+          'Pillar',
+          'Dawn',
+          'Noon',
+          'Evening',
+          'Night',
+          'Midnight',
+          'Spring',
+          'Summer',
+          'Fall',
+          'Autumn',
+          'Winter',
+          'Beginning',
+          'Ending',
+          'End'
+        ];
+        
+        $adjt = [
+          'Unyielding',
+          'Destructive',
+          'Valiant',
+          'Unending',
+          'Unbending'
+        ];
+        
+        return $adjt[mt_rand(0, count($adjt) - 1)] . ' ' .
+               $noun[mt_rand(0, count($noun) - 1)];
+      
+      case 1:
+        $noun = [
+          'Pillar',
+          'Dawn',
+          'Noon',
+          'Evening',
+          'Night',
+          'Midnight',
+          'Spring',
+          'Summer',
+          'Fall',
+          'Autumn',
+          'Winter',
+          'Beginning',
+          'Ending',
+          'End'
+        ];
+        
+        return $noun[mt_rand(0, count($noun) - 1)] . ' of ' .
+               $noun[mt_rand(0, count($noun) - 1)];
+    }
+    
     return 'Not implemented';
   }
   
