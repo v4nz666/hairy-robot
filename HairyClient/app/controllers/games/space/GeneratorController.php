c269cb8bbecf119cb58bdc8056fce03f84cb85c6
Corey Frenette <corey@narwhunderful.com>
2013-12-07 10:45:40 -0400

    Move name gens into a helper class that gets autoloaded SEE NOTES
    YOU NEED TO RUN "composer dump-autoload"

--- HairyClient/app/controllers/games/space/GeneratorController.php
+++ HairyClient/app/controllers/games/space/GeneratorController.php
@@ -2,84 +2,19 @@
 
 namespace Games\Space;
 
+use Hairy\Gen;
+
 class GeneratorController extends \Controller {
   public function __construct() {
     $this->beforeFilter('auth');
   }
   
   public function ships() {
-    switch(mt_rand(0, 1)) {
-      case 0:
-        $noun = [
-          'Pillar',
-          'Dawn',
-          'Noon',
-          'Evening',
-          'Night',
-          'Midnight',
-          'Spring',
-          'Summer',
-          'Fall',
-          'Autumn',
-          'Winter',
-          'Beginning',
-          'Ending',
-          'End'
-        ];
-        
-        $adjt = [
-          'Unyielding',
-          'Destructive',
-          'Valiant',
-          'Unending',
-          'Unbending'
-        ];
-        
-        return $adjt[mt_rand(0, count($adjt) - 1)] . ' ' .
-               $noun[mt_rand(0, count($noun) - 1)];
-      
-      case 1:
-        $noun = [
-          'Pillar',
-          'Dawn',
-          'Noon',
-          'Evening',
-          'Night',
-          'Midnight',
-          'Spring',
-          'Summer',
-          'Fall',
-          'Autumn',
-          'Winter',
-          'Beginning',
-          'Ending',
-          'End'
-        ];
-        
-        return $noun[mt_rand(0, count($noun) - 1)] . ' of ' .
-               $noun[mt_rand(0, count($noun) - 1)];
-    }
-    
-    return 'Not implemented';
+    return Gen::generateShip();
   }
   
   public function planets() {
-    $name = [
-      'Ceres', 'Pallas', 'Juno', 'Vesta', 'Astraea', 'Hebe', 'Iris', 'Flora', 'Metis',
-      'Hygiea', 'Parthenope', 'Victoria', 'Egeria', 'Irene', 'Eunomia', 'Psyche', 'Tethis',
-      'Melpomene', 'Fortuna', 'Massalia', 'Lutetia', 'Kalliope', 'Thalia', 'Themis', 'Procaea',
-      'Proserpina', 'Euterpe', 'Bellona', 'Amphitrite', 'Urania', 'Euphrosyne', 'Pomona',
-      'Polyhymnia', 'Circe', 'Leukothea', 'Atalante', 'Fides', 'Leda', 'Laetitia', 'Harmonia',
-      'Daphne', 'Isis', 'Ariadne', 'Nysa', 'Eugenia', 'Hestia', 'Aglaja', 'Doris', 'Pales', 'Virginia'
-    ];
-    
-    $n = $name[mt_rand(0, count($name) - 1)];
-    
-    if(mt_rand(0, 4) === 0) {
-      $n = 'New ' . $n;
-    }
-    
-    return $n;
+    return Gen::generatePlanet();
   }
   
   // Damn, this was a good shot at splitting words into syllables, but won't work...
---
+++ HairyClient/app/helpers/Hairy/Gen.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Hairy;
+
+class Gen {
+  public static function generateShip() {
+    switch(mt_rand(0, 1)) {
+      case 0:
+        $noun = [
+          'Pillar',
+          'Dawn',
+          'Noon',
+          'Evening',
+          'Night',
+          'Midnight',
+          'Spring',
+          'Summer',
+          'Fall',
+          'Autumn',
+          'Winter',
+          'Beginning',
+          'Ending',
+          'End'
+        ];
+        
+        $adjt = [
+          'Unyielding',
+          'Destructive',
+          'Valiant',
+          'Unending',
+          'Unbending'
+        ];
+        
+        return $adjt[mt_rand(0, count($adjt) - 1)] . ' ' .
+               $noun[mt_rand(0, count($noun) - 1)];
+      
+      case 1:
+        $noun = [
+          'Pillar',
+          'Dawn',
+          'Noon',
+          'Evening',
+          'Night',
+          'Midnight',
+          'Spring',
+          'Summer',
+          'Fall',
+          'Autumn',
+          'Winter',
+          'Beginning',
+          'Ending',
+          'End'
+        ];
+        
+        return $noun[mt_rand(0, count($noun) - 1)] . ' of ' .
+               $noun[mt_rand(0, count($noun) - 1)];
+    }
+    
+    return 'Not implemented';
+  }
+  
+  public static function generatePlanet() {
+    $name = [
+      'Ceres', 'Pallas', 'Juno', 'Vesta', 'Astraea', 'Hebe', 'Iris', 'Flora', 'Metis',
+      'Hygiea', 'Parthenope', 'Victoria', 'Egeria', 'Irene', 'Eunomia', 'Psyche', 'Tethis',
+      'Melpomene', 'Fortuna', 'Massalia', 'Lutetia', 'Kalliope', 'Thalia', 'Themis', 'Procaea',
+      'Proserpina', 'Euterpe', 'Bellona', 'Amphitrite', 'Urania', 'Euphrosyne', 'Pomona',
+      'Polyhymnia', 'Circe', 'Leukothea', 'Atalante', 'Fides', 'Leda', 'Laetitia', 'Harmonia',
+      'Daphne', 'Isis', 'Ariadne', 'Nysa', 'Eugenia', 'Hestia', 'Aglaja', 'Doris', 'Pales', 'Virginia'
+    ];
+    
+    $n = $name[mt_rand(0, count($name) - 1)];
+    
+    if(mt_rand(0, 4) === 0) {
+      $n = 'New ' . $n;
+    }
+    
+    return $n;
+  }
+}
