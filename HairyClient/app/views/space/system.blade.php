e364148b39c448d7e1a746261882247e773ccbb2
Corey Frenette <corey@narwhunderful.com>
2013-12-07 09:37:21 -0400

    Mostly functional solar system view

--- HairyClient/app/views/space/system.blade.php
+++ HairyClient/app/views/space/system.blade.php
@@ -4,6 +4,122 @@
   <head>
     <meta charset="UTF-8">
     <title>Hairy Robot</title>
+    {{ HTML::style('assets/css/system2.css') }}
+    
+    <style>
+      /* --------------------------------------------------------------------------- planets index */
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      #planet{{ $i }} {
+        z-index: {{ count($planets) - $i + 1 }};
+      }
+      @endfor
+      
+      #star {
+        z-index: 1;
+      }
+      
+      /* --------------------------------------------------------------------------- data */
+      /* --------------------------------------------------------------------------- speed */
+      /* sideral years */
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      #planet{{ $i }} .pos,
+      #planet{{ $i }} .planet,
+      #planet{{ $i }}.orbit {
+        animation-duration: {{ 12.00021 + ($i - 2) * 4 }}s; /***** TODO *****/
+      }
+      @endfor
+      
+      /* --------------------------------------------------------------------------- planets sizes */
+      /* --------------------------------------------------------------------------- stretched sizes */
+      .scale-stretched #star {
+        font-size: 24em;
+      }
+      
+      <?php $scale = 3.92 / $planets[2]->size; ?> /* TODO this isn't so great */
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      .scale-stretched #planet{{ $i }} .planet {
+        font-size: {{ min(12, $planets[$i]->size * $scale) }}em;
+      }
+      @endfor
+      
+      /* --------------------------------------------------------------------------- stretched orbits */
+      <?php $s = 32; $last = 0; $scale = 3.92 / $planets[2]->size; ?> /* TODO this isn't so great */
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      .scale-stretched #planet{{ $i }}.orbit {
+        <?php $size = min(12, $planets[$i]->size * $scale) + 2; $s += $size + $last; $last = $size; ?>
+        width: {{ $s }}em;
+        height: {{ $s }}em;
+        margin-top: {{ $s / -2 }}em;
+        margin-left: {{ $s / -2 }}em;
+      }
+      @endfor
+      
+      /* --------------------------------------------------------------------------- text infos data */
+      /* --------------------------------------------------------------------------- speed */
+      .set-speed dl.infos dd span:after {
+        content: 'Orbit Velocity'; }
+      
+      .set-speed #star dl.infos dd:after {
+        content: '0 km/h'; }
+      
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      .set-speed #planet{{ $i }} dl.infos dd:after {
+        content: '107,218 km/h'; } /***** TODO *****/
+      @endfor
+      
+      /* --------------------------------------------------------------------------- size */
+      .set-size dl.infos dd span:after {
+        content: 'Equatorial Circumference'; }
+      
+      .set-size #star dl.infos dd:after {
+        content: '4,370,005 km'; } /***** TODO *****/
+      
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      .set-size #planet{{ $i }} dl.infos dd:after {
+        content: '40,030 km'; } /***** TODO *****/
+      @endfor
+      
+      /* --------------------------------------------------------------------------- distance */
+      .set-distance dl.infos dd span:after {
+        content: 'From Sun'; } /***** TODO *****/
+      
+      .set-distance #star dl.infos dd span:after {
+        content: 'From Earth'; } /***** TODO *****/
+      
+      .set-distance #star dl.infos dd:after {
+        content: '149,598,262 km'; } /***** TODO *****/
+      
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      .set-distance #planet{{ $i }} dl.infos dd:after {
+        content: '149,598,262 km'; } /***** TODO *****/
+      @endfor
+      
+      .star #star .infos,
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      .planet{{ $i }} #planet{{ $i }} .infos,
+      @endfor
+      {
+        display: block;
+        opacity: 1;
+        transform: rotateX(0deg);
+      }
+
+      @for($i = 0; $i < count($planets); $i++)
+      <?php if($planets[$i]->type !== 'planet') continue; ?>
+      .planet{{ $i }} #planet{{ $i }}.orbit,
+      @endfor
+      {
+        border: 1px solid rgba(255, 255, 255, 0.8);
+      }
+    </style>
   </head>
   
   <body class="opening hide-UI view-2D zoom-large data-close controls-close">
